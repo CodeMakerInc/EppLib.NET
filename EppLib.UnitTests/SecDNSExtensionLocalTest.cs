448cb4ff097869146494880d9620e0cdeccde53e
Ross Pace <rpace@avantia-inc.com>
2016-10-14 12:41:43 -0400

    secDNS extensions

---
+++ EppLib.UnitTests/SecDNSExtensionLocalTest.cs
@@ -0,0 +1,79 @@
+﻿using EppLib.Entities;
+using EppLib.Extensions.SecDNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EppLib.Tests
+{
+    [TestClass]
+    public class SecDNSExtensionLocalTest
+    {
+        [TestMethod]
+        [TestCategory("SecDNSExtension")]
+        [DeploymentItem("TestData/SecDNSCreateExtension.xml")]
+        public void SecDNSCreateExtension()
+        {
+            string expected = File.ReadAllText("SecDNSCreateExtension.xml");
+
+            var command = new DomainCreate("example1", "jd1234");
+            command.Period = new DomainPeriod(2, "y");
+            command.NameServers.Add("ns1.example.net");
+            command.NameServers.Add("ns2.example.net");
+            command.DomainContacts.Add(new DomainContact("sh8013", "admin"));
+            command.DomainContacts.Add(new DomainContact("sh8013", "tech"));
+            command.Password = "2fooBAR";
+            command.TransactionId = "ABC-12345";
+
+            // create extension
+            var extension = new SecDNSCreate { MaxSigLife = 604800 };
+            extension.DsData.Add(new SecDNSData {
+                KeyTag = 12345,
+                Algorithm = SecDNSAlgorithm.RSASHA1,
+                Digest = "49FD46E6C4B45C55D4AC"
+            });
+
+            command.Extensions.Add(extension);
+
+            var xml = command.ToXml().InnerXml;
+
+            Assert.AreEqual(expected, xml);
+        }
+
+        [TestMethod]
+        [TestCategory("SecDNSExtension")]
+        [DeploymentItem("TestData/SecDNSUpdateExtension.xml")]
+        public void SecDNSUpdateExtension()
+        {
+            string expected = File.ReadAllText("SecDNSUpdateExtension.xml");
+
+            var command = new DomainUpdate("example1");
+
+            var extension = new SecDNSUpdate();
+
+            // remove
+            extension.ToRemove.Add(new SecDNSData {
+                KeyTag = 12345,
+                Algorithm = SecDNSAlgorithm.RSASHA1,
+                Digest = "49FD46E6C4B45C55D4AC"
+            });
+
+            // add
+            extension.ToAdd.Add(new SecDNSData
+            {
+                KeyTag = 12346,
+                Algorithm = SecDNSAlgorithm.RSASHA1,
+                Digest = "38EC35D5B3A34B44C39B"
+            });
+
+            command.Extensions.Add(extension);
+
+            var xml = command.ToXml().InnerXml;
+
+            Assert.AreEqual(expected, xml);
+        }
+    }
+}
---
+++ EppLib/Extensions/SecDNS/SecDNSData.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml;
+
+namespace EppLib.Extensions.SecDNS
+{
+    public enum SecDNSAlgorithm
+    {
+        RSAMD5 = 1,
+        DH = 2,
+        DSA = 3,
+        ECC = 4,
+        RSASHA1 = 5,
+        INDIRECT = 252,
+        PRIVATEDNS = 253,
+        PRIVATEOID = 254
+    }
+
+    public class SecDNSData
+    {
+        public short KeyTag { get; set; }
+        public SecDNSAlgorithm Algorithm { get; set; }
+        public string Digest { get; set; }
+
+        public XmlNode ToXml(XmlDocument doc)
+        {
+            var dataNode = doc.CreateElement("secDNS:dsData", "urn:ietf:params:xml:ns:secDNS-1.1");
+
+            var keyTagNode = doc.CreateElement("secDNS:keyTag", "urn:ietf:params:xml:ns:secDNS-1.1");
+            keyTagNode.InnerText = KeyTag.ToString();
+            dataNode.AppendChild(keyTagNode);
+
+            var algNode = doc.CreateElement("secDNS:alg", "urn:ietf:params:xml:ns:secDNS-1.1");
+            algNode.InnerText = ((int)Algorithm).ToString();
+            dataNode.AppendChild(algNode);
+
+            var digestTypeNode = doc.CreateElement("secDNS:digestType", "urn:ietf:params:xml:ns:secDNS-1.1");
+            digestTypeNode.InnerText = "1";
+            dataNode.AppendChild(digestTypeNode);
+
+            var digestNode = doc.CreateElement("secDNS:digest", "urn:ietf:params:xml:ns:secDNS-1.1");
+            digestNode.InnerText = Digest;
+            dataNode.AppendChild(digestNode);
+
+            return dataNode;
+        }
+    }
+}
