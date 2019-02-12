cabadb71aa78872edfc0de125b43696c475fa522
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2019-02-12 11:55:41 -0500

    IDN extension + test

---
+++ EppLib.UnitTests/IdnExtensionLocalTest.cs
@@ -0,0 +1,31 @@
+﻿using EppLib.Entities;
+using EppLib.Extensions.Idn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace EppLib.Tests
+{
+    [TestClass]
+    public class IdnExtensionLocalTest
+    {
+        [TestMethod]
+        [TestCategory("FuryExtension")]
+        [DeploymentItem("TestData/IdnDomainCreateCommand.xml")]
+        public void Test()
+        {
+            string expected = File.ReadAllText("IdnDomainCreateCommand.xml");
+
+            var command = new DomainCreate("xn--espaol-zwa.example.com", "jd1234");
+            command.Password = "2fooBAR";
+            command.DomainContacts.Add(new DomainContact("sh8013", "admin"));
+            command.DomainContacts.Add(new DomainContact("sh8013", "tech"));
+
+            command.Extensions.Add(new IdnExtension("es", "español.example.com"));
+
+            var xml = command.ToXml().InnerXml;
+
+            Assert.AreEqual(expected, xml);
+        }
+        
+    }
+}
---
+++ EppLib/Extensions/Idn/IdnExtension.cs
@@ -0,0 +1,35 @@
+﻿using EppLib.Entities;
+using System.Xml;
+
+namespace EppLib.Extensions.Idn
+{
+    public class IdnExtension : EppExtension
+    {
+        private string _ns = "urn:ietf:params:xml:ns:idn-1.0";
+
+        private string table;
+        private string uname;
+
+        public IdnExtension(string table, string uname)
+        {
+            this.table = table;
+            this.uname = uname;
+        }
+
+        protected override string Namespace
+        {
+            get { return _ns; }
+            set { _ns = value; }
+        }
+
+        public override XmlNode ToXml(XmlDocument doc)
+        {
+            var root = CreateElement(doc, "idn:data");
+
+            AddXmlElement(doc, root, "idn:table", table);
+            AddXmlElement(doc, root, "idn:uname", uname);
+
+            return root;
+        }
+    }
+}
