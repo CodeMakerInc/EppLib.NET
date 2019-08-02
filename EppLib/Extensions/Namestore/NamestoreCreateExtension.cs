3a26b79d20390cec2cd0b95045580b1c4648b146
Nerzool <nerzool@hotmail.com>
2019-08-03 01:40:25 +0200

    Namestore extension (.com/.net/.cc/...)

---
+++ EppLib/Extensions/Namestore/NamestoreCreateExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace EppLib.Extensions.Namestore
+{
+    public class NamestoreCreateExtension : NamestoreExtension
+    {
+        public NamestoreCreateExtension(string subproduct)
+        {
+            SubProduct = subproduct;
+        }
+
+        public string SubProduct;
+
+        public override XmlNode ToXml(XmlDocument doc)
+        {
+            var root = CreateElement(doc, "namestoreExt");
+
+            if (SubProduct != null)
+            {
+                AddXmlElement(doc, root, "namestoreExt:subProduct", $"dot{SubProduct}");
+            }
+
+            return root;
+        }
+    }
+}
---
+++ EppLib/Extensions/Namestore/NamestoreExtension.cs
@@ -0,0 +1,18 @@
+﻿using EppLib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace EppLib.Extensions.Namestore
+{
+    public abstract class NamestoreExtension : EppExtension
+    {
+        private string _ns = "http://www.verisign-grs.com/epp/namestoreExt-1.1";
+        protected override string Namespace
+        {
+            get { return _ns; }
+            set { _ns = value; }
+        }
+    }
+}
