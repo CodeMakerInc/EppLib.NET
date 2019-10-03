dea6e0a5ad980bf4e8ec63ceedeed5b6718d9da2
vdm <dutchpowercow@gmail.com>
2019-10-03 12:33:54 +0200

    Abstracted away connection & read/write implementation

---
+++ EppLib/ITransport.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2012 Code Maker Inc. (http://codemaker.net)
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using System;
+using System.Xml;
+/**/
+using System.Security.Authentication;
+
+
+/*
+using OpenSSL;
+using OpenSSL.Core;
+using OpenSSL.X509;*/
+
+
+namespace EppLib
+{
+    public interface ITransport : IDisposable
+    {
+        void Connect(SslProtocols sslProtocols = SslProtocols.Tls);
+        void Disconnect();
+        void Write(XmlDocument xmlDocument);
+        byte[] Read();
+    }
+}
